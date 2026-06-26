25c5c133b29481c0037d14bd124891fee6135015
Enes <ereseres1579@gmail.com>
2026-06-26 23:57:30 +0300

    Add API Gateway routes and set gateway port
    Add RouterFunction-based routes to the api-gateway for user, device, ingestion and insight services (UserServiceRoutes, DeviceServiceRoutes, IngestionServiceRoutes, insightServiceRoutes). Each route forwards /api/v1/user/** -> http://localhost:8080, /api/v1/devices/** -> http://localhost:8081, /api/v1/ingestion/** -> http://localhost:8082, /api/v1/insights/** -> http://localhost:8085 using Spring Cloud Gateway MVC handler/filter functions. Also set api-gateway server.port=9000 in application.properties.

---
+++ api-gateway/src/main/java/com/vena/apigateway/route/insightServiceRoutes.java
@@ -0,0 +1,25 @@
+package com.vena.apigateway.route;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+
+@Configuration
+public class insightServiceRoutes {
+
+
+    @Bean
+    public RouterFunction<ServerResponse> insightRoute(){
+        return route("insight-service")
+                .route(RequestPredicates.path("/api/v1/insights/**"), http())
+                .before(uri("http://localhost:8085"))
+                .build();
+
+    }
+}
